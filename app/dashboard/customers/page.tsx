1dee7830d11bc7dcabac006370b35da876fafca6
sorrego98 <juan.sorrego98@gmail.com>
2023-11-19 10:11:19 -0500

    Changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const customer = () => {
+  return (
+    <div>customer page</div>
+  )
+}
+
+export default customer
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer>
+          Prueba de Layout FOOTER
+        </footer>
+      </body>
     </html>
   );
 }
